9dfe6eb3d474d4510258e537faeb41c34269de5d
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2022-02-14 22:10:15 +0100

    added Reading List with contexst and reducer

---
+++ src/components/BookForm.js
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from "react";
+import { BookContext } from "../context/BookContext";
+
+const BookForm = () => {
+  const { dispatch } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError("");
+    if (!title.trim() || !author.trim()) {
+      setError("you need to insert text in all fields");
+      return;
+    }
+
+    dispatch({
+      type: "ADD_BOOK",
+      payload: {
+        title,
+        author,
+      },
+    });
+
+    setTitle("");
+    setAuthor("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error ? <strong>{error}</strong> : null}
+      <input
+        type="text"
+        placeholder="Book title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Author name"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+
+      <input type="submit" value="Add book" />
+    </form>
+  );
+};
+
+export default BookForm;
